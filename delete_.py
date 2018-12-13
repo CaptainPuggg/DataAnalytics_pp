61a709966822049765bd8696dbacff6f6cf3234a
CaptainPuggg <gloria_mei322@yahoo.com>
2018-12-13 15:09:02 +0800

    Update delete_.py

--- delete_.py
+++ delete_.py
@@ -1,23 +1,11 @@
 
 # coding: utf-8
 
-# In[11]:
-
-
 import pandas as pd
-
-
-# In[12]:
-
 
 bigdf=pd.read_csv('Big.csv')
 smalldf=pd.read_csv('Small.csv')
 
-
-# In[22]:
-
-
 for item in bigdf.columns:
     if item not in smalldf.columns:
         bigdf.drop(str(item), axis=1, inplace=True)
-
